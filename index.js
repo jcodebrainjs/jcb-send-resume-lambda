6b2535632f5800aacad05c3b85462b31026f4b81
JCodeBrain <david@jcodebrain.com>
2019-04-11 07:11:46 -0700

    minor fixes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const aws = require('aws-sdk')
 const nodemailer = require('nodemailer')
 
-const ses = new aws.SES()
+const ses = new aws.SES({ region: 'us-west-2'})
 const s3 = new aws.S3()
 
 function getS3File(bucket, key) {
@@ -24,14 +24,14 @@ function getS3File(bucket, key) {
 exports.handler = (event, context, callback) => {
   getS3File(
     'jcb-ses-attachments',
-    'Resume-David-Cotelessa-JavascriptDev-20190408.pdf'
+    `Resume-David-Cotelessa-JavascriptDev-20190408.${event.doctype}` 
   )
     .then(fileData => {
       const mailOptions = {
         from: 'david@jcodebrain.com',
         subject: 'Thank you for requesting my resume.',
-        html: `<p>I'm David Cotelessa and I'm sending you my resume because you went to my website (generated from Hexo) and clicked on a request: THANK YOU for your interest in my skills. Did you know this email was sent by Amazon Web Services, using SES, a Lambda created in Node, and nodemailer? If you'd like to see, check out my <a href="http://github.com/jcodebrainjs">github repo</a>.</p><p>If it's okay, I'd like to update my resume with you every so often in case of any exciting opportunites that may be a good match. (Don't worry, I'm not that clingy.)</p><p>Attached is my latest resume in the format you requested, if you have a need for someone of my skill set, please reply back and we'll discuss.</p><p>Cheers,<br />David Cotelessa<br>323 829 0272</p>`,
-        to: 'david@jcodebrain.com',
+        html: `<p>I'm David Cotelessa and I'm sending you my resume because you went to my website (generated from Hexo) and clicked on a request: THANK YOU for your interest in my skills. Did you know this email was sent by Amazon Web Services, using SES, a Lambda created in Node, and nodemailer? If you'd like to see, check out my <a href="http://github.com/jcodebrainjs">github repo</a>.</p><p>If it's okay, I'd like to update my resume with you every so often in case of any exciting opportunites that may be a good match.</p><p>Attached is my latest resume in the format you requested, if you have a need for someone of my skill set, please reply back and we'll discuss.</p><p>Cheers,<br />David Cotelessa<br>323 829 0272</p>`,
+        to: `${event.emailname}@${event.emaildomain}`,
         attachments: [
           {
             filename: 'Resume-JavascriptDev-David-Cotelessa.pdf',
@@ -45,14 +45,14 @@ exports.handler = (event, context, callback) => {
         SES: ses
       })
 
-      transporter.sendMail(mailOptions, (err, info) => {
+      transporter.sendMail(mailOptions, (err) => {
         if (err) {
           console.log(err)
           console.log('Error sending email!')
           callback(err)
         } else {
           console.log('Email sent.')
-          callback()
+          callback({success: true })
         }
       })
     })
